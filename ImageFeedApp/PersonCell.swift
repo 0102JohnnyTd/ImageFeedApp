f77cb828817bbb19ef6a89af55d02f406351c2f3
0102johnny <rt2824.recruit.1022@gmail.com>
2023-01-10 16:32:14 +0900

    FriendCollectionViewControllerに表示するCellのクラスを定義

---
+++ ImageFeedApp/PersonCell.swift
@@ -0,0 +1,13 @@
+//
+//  PersonCell.swift
+//  ImageFeedApp
+//
+//  Created by Johnny Toda on 2023/01/10.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    // セルのID
+    static let identifier = "kListCollectionViewCell"
+}
