b62dba9d6bca98735e18017f24ef360f71d310e5
0102johnny <rt2824.recruit.1022@gmail.com>
2023-01-10 16:30:40 +0900

    extensionを用いてUIColorクラスにアプリの背景色を追加

---
+++ ImageFeedApp/Utilities.swift
@@ -0,0 +1,12 @@
+//
+//  Utilities.swift
+//  ImageFeedApp
+//
+//  Created by Johnny Toda on 2023/01/10.
+//
+
+import UIKit
+
+extension UIColor {
+    static let appBackgroundColor = #colorLiteral(red: 0.05882352941, green: 0.09019607843, blue: 0.1098039216, alpha: 1)
+}
