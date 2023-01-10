e6b0bf0e6e2edb0b60b80017d406fa82ac494e37
0102johnny <rt2824.recruit.1022@gmail.com>
2023-01-10 16:33:06 +0900

    FriendViewControllerに表示させるItemのレイアウトを構築するクラスを定義

---
+++ ImageFeedApp/ColumnFlowLayout.swift
@@ -0,0 +1,15 @@
+//
+//  ColumnFlowLayout.swift
+//  ImageFeedApp
+//
+//  Created by Johnny Toda on 2023/01/08.
+//
+
+import UIKit
+
+// FriendViewControllerに表示させるItemのレイアウトを構築するクラス
+final class ColumnFlowLayout: UICollectionViewFlowLayout {
+    override func prepare() {
+        super.prepare()
+    }
+}
