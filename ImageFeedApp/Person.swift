98554ef45865e11d019887269f88c435e3d6492f
0102johnny <rt2824.recruit.1022@gmail.com>
2023-01-10 19:30:20 +0900

    PersonCellに表示するPersonのモデルを定義

---
+++ ImageFeedApp/Person.swift
@@ -0,0 +1,46 @@
+//
+//  Person.swift
+//  ImageFeedApp
+//
+//  Created by Johnny Toda on 2023/01/10.
+//
+
+import Foundation
+
+
+// Personの更新パターンを管理するenum
+enum PersonUpdate {
+    case delete(Int)
+    case insert(Person, Int)
+    case move(Int, Int)
+    case reload(Int)
+}
+
+// PersonCellに表示するPersonのモデル
+struct Person: CustomStringConvertible {
+    var name: String?
+    var imgName: String?
+    var lastUpdate = Date()
+
+    init(name: String, day: Int, month: Int, year: Int) {
+        self.name = name
+        self.imgName = name.lowercased()
+
+        var components = DateComponents()
+        components.day = day
+        components.month = month
+        components.year = year
+
+        if let date = Calendar.current.date(from: components) {
+            lastUpdate = date
+        }
+    }
+
+    var description: String {
+        if let name = self.name {
+            return "<\(type(of: self)): name = \(name)>"
+        } else {
+            return "<\(type(of: self))>"
+        }
+    }
+}
