724f2f4dbcb16628fb86ca71caa7e5f79848f769
0102johnny <rt2824.recruit.1022@gmail.com>
2023-01-14 17:10:35 +0900

    FriendViewController上のCollectionViewCellのレイアウトを設定するコードを実装

--- ImageFeedApp/Layouts/ColumnFlowLayout.swift
+++ ImageFeedApp/Layouts/ColumnFlowLayout.swift
@@ -11,5 +11,18 @@ import UIKit
 final class ColumnFlowLayout: UICollectionViewFlowLayout {
     override func prepare() {
         super.prepare()
+
+        // アンラップ
+        guard let cv = collectionView else { return }
+
+        // 🍏左右両端+8した上での残りの幅の値を返す値？
+        self.itemSize = CGSize(width: cv.bounds.inset(by: cv.layoutMargins).size.width, height: 70.0)
+
+        // Section間の間隔を設定
+        self.sectionInset = UIEdgeInsets(top: self.minimumInteritemSpacing, left: .zero, bottom: .zero, right: .zero)
+
+        // 記述することでデバイスを横向きにした際にTrailing,LeadingにAutoLayoutを対応させる。
+        self.sectionInsetReference = .fromSafeArea
     }
 }
+
